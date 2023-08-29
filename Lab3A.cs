681a09c92b0934c3769df7b0ccb44ddb2cc016c5
Sparsegull2 <142937470+Sparsegull2@users.noreply.github.com>
2023-08-29 17:53:20 -0400

    Add files via upload

---
+++ Lab3A.cs
@@ -0,0 +1,26 @@
+﻿/* 
+Class: CSE 1321L 
+Section: #03 
+Term: Fall 2023
+Instructor: John Blake 
+Name: Christopher Morrison
+Lab#: Lab3A 
+*/
+using System;
+
+class Lab3A
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Amount owed: ");
+        float balance = float.Parse(Console.ReadLine());
+        Console.Write("APR: ");
+        float APR = float.Parse(Console.ReadLine());
+        float mpr = APR / 12;
+        float min = (mpr / 100) * balance;
+        Console.WriteLine("Monthly percentage rate: " + mpr.ToString("n3"));
+        Console.WriteLine("Minimum payment: $" + min.ToString("n2"));
+
+    }
+
+}
